2f7e1390a880d8ac4a851d3212d5857a04849fd6
Gustavo Olivares <guolivar@gmail.com>
2023-08-28 12:06:36 +1200

    Not sure what had happened

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
-#!/usr/bin/env python
+import setuptools
 
-import ez_setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-ez_setup.use_setuptools()
-
-from setuptools import setup, find_packages
-setup(
-    name = "sell",
-    version = "0.1",
-    packages = find_packages(),
-    license='MIT',
-    long_description=open('README.md').read(),
+setuptools.setup(
+    name="serial-logger-niwa", # Replace with your own username
+    version="0.0.1",
+    author="Gustavo Olivares",
+    author_email="gustavo.olivares@niwa.co.nz",
+    description="A logger for serial communicating devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/niwa/serial_listener_logger",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     install_requires = ['pyserial'],
     extras_require = {
         'tests': ['nose >= 1.0']
